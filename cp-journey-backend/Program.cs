b0e1bfc6afff0c90192be15b23463e2bce42e8b6
MaxwellOliveira01 <maxwell1de1oliveira@gmail.com>
2025-06-27 20:45:08 -0300

    downgrading .NET to 8 and add EF

--- cp-journey-backend/Program.cs
+++ cp-journey-backend/Program.cs
@@ -1,38 +1,46 @@
+using cp_journey_backend.Entities;
 using cp_journey_backend.Persistence;
 using cp_journey_backend.Repositories;
 using cp_journey_backend.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 builder.Services.AddScoped<IProfileService, ProfileService>();
 
-// refatorar isso aqui? criar um appDbContext unico
-// e nele eu injeto o InMemoryDb ou o normal
-// mas dai nao preciso de duas implementações pra cada repositorio
 builder.Services.AddScoped<IProfileRepository, InMemoryProfileRepository>();
 builder.Services.AddScoped<IUniversityRepository, InMemoryUniversityRepository>();
 
 builder.Services.AddSingleton<ModelConverter>();
 builder.Services.AddSingleton<InMemoryDb>();
 
-var app = builder.Build();
+builder.Configuration.AddUserSecrets<Program>();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+var databasePath = Path.Combine(builder.Environment.ContentRootPath, "Database");
+
+Directory.CreateDirectory(databasePath);
+
+var connectionString = $"Data Source={Path.Combine(databasePath, "app.db")}";
+
+builder.Services.AddDbContext<AppDbContext>(options 
+    => options.UseSqlite(connectionString));
+
+var app = builder.Build();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Apply the remaining migrations before starting the application
+using (var scope = app.Services.CreateScope()) {
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
---
+++ cp-journey-backend/Services/AppDbContext.cs
@@ -0,0 +1,9 @@
+namespace cp_journey_backend.Services;
+
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options) {
+    
+    
+ 
+}
